2c7602f3ae6f2cc903c617d4b5f3a455d843d64a
Pedro Simões <pedesim@users.noreply.github.com>
2026-05-08 23:18:33 +0800

    Add personal notes on confirmed working language codes

--- __init__.py
+++ __init__.py
@@ -8,4 +8,10 @@ except ImportError:
 
 # Personal note: BACKBONE_LANGUAGE_MAP maps language codes to backbone model names.
 # Useful reference when adding support for new languages in downstream projects.
+#
+# Language codes I've confirmed working in my projects:
+#   'en' - English (most stable)
+#   'de' - German
+#   'fr' - French
+# See https://github.com/neuphonic/neutts for the full list.
 __all__ = ["NeuTTS", "BACKBONE_LANGUAGE_MAP", "__version__"]
